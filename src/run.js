00a56ecff97d01e30a8df600c5376107598ecba6
Albin Hubsch <albin.hubsch@gmail.com>
2019-01-04 22:07:39 +0100

    Added a few help commands

--- src/run.js
+++ src/run.js
@@ -2,6 +2,7 @@ const setup = require('./setup')
 const parse = require('./parse')
 const post = require('./post')
 const config = require('./config')
+const chalk = require('chalk')
 
 async function run(args){
 
@@ -9,6 +10,23 @@ async function run(args){
     if(args[0] === 'setup' || !config.hasConfig()){
         // RUN SETUP PROCESS
         await setup()
+        return true
+    }
+
+    if (args[0] === 'help' || !config.hasConfig()) {
+        // RUN SETUP PROCESS
+        console.log(chalk.blue("\n setup - Run setup process\n projects - Lists all available projects that can be time reported\n <project> <duration> [<date(today, idag, yesterday, igår, YYYY-MM-DD)>]\n"));
+        return true
+    }
+
+    if (args[0] === 'projects' || !config.hasConfig()) {
+        // RUN SETUP PROCESS
+        const conf = config.getConfig()
+        console.log('')
+        conf.projects.map( project => {
+            console.log(chalk.blue(` - ${project.title}`))
+        })
+        console.log('')
         return true
     }
 
